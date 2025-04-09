509d88b64ef167a01db0f4733c6834c54792d555
Shawnm-MAD <shawnmathew14@outlook.com>
2025-04-09 13:55:24 -0400

    Added firebase dependencies for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
